2b97e7188b88d543ead95182f62eb95373af19e4
nanutnutna <jtnattapot@gmail.com>
2024-06-04 00:26:42 +0700

    tic-tac-toe

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ WIDTH = 600
 HIGHT = 600
 CH = 3
 SCALE = WIDTH/CH
+RADIUS = SCALE/2*(3/4)
+LINE_WIDTH = 10
 frame = pygame.display.set_mode((WIDTH,HIGHT))
 
 board = [[None]*CH for _ in range(CH)]
@@ -25,14 +27,19 @@ def draw_grid():
     for i in range(1,3):
         pygame.draw.line(frame,color,(0,WIDTH*i//3),(HIGHT,WIDTH*i//3),width=6)
 
-def draw_symbol(x,y,player):
-    color = (255,255,255)   
-    if player == "X":
-        pass
+def draw_symbol(x_,y_,player):
+    point = {(0,0):(1,1),(0,1):(1,3),(0,2):(1,5),(1,0):(3,1),(1,1):(3,3),(1,2):(3,5),(2,0):(5,1),(2,1):(5,3),(2,2):(5,5)}
+    x,y = point[(x_,y_)]
+    color = (255,255,255)
+    if player == 'O':
+        pygame.draw.circle(frame,color,(HIGHT*x//6,WIDTH*y//6),width=LINE_WIDTH,radius=RADIUS)
     else:
-        pygame.draw.circle(frame,color,(x*SCALE,y*SCALE),radius=50)
-        
+        p1,p2 = (HIGHT*x//6,WIDTH*y//6)
+        pygame.draw.lines(frame, color, True, [(p1-RADIUS,p2-RADIUS),(p1+RADIUS,p2+RADIUS)], width=LINE_WIDTH)
+        pygame.draw.lines(frame, color, True, [(p1-RADIUS,p2+RADIUS),(p1+RADIUS,p2-RADIUS)], width=LINE_WIDTH)
 
+def check_win():
+    pass
 
 running = True
 draw_grid()
